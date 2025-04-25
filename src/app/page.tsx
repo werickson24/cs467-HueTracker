a47c5abc65cd8927d0199fcfd998385e9b073f93
Wil <william@ericksonls.com>
2025-04-25 10:12:09 -0700

    Add debug launch file, tweak branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to HueTracker.</strong>
           </p>
           <Link
             href="/login"
